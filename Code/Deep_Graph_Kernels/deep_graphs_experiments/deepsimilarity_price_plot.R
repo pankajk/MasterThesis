a3dbcefd88fb778f24ad5e57315330b003d9319f
Pankaj Kumar <kumar.x.pankaj@gmail.com>
2016-03-21 12:35:59 +0300

    deep graph kernels added

--- Code/Deep_Graph_Kernels/deep_graphs_experiments/deepsimilarity_price_plot.R
+++ Code/Deep_Graph_Kernels/deep_graphs_experiments/deepsimilarity_price_plot.R
@@ -1,3 +1,5 @@
+## Deep kernels similariry Index and price correlation
+
 # Setting working dir
 getwd()
 setwd("/home/pankaj/MasterThesis")
@@ -12,23 +14,21 @@ library(reshape)
 # read data
 P=read.csv(file="./data/btc_usd/daily_price_march_april.csv",head=TRUE,sep=",")
 
-P=read.csv(file="./data/btc_usd/daily_price_2009_2016.csv",head=TRUE,sep=",")
-
 colnames(P) <- tolower(colnames(P))
 
 P$time <- as.Date( as.character( P$time ) )
 names(P)
 P
 
-cor(P$log_close,P$sim_index)
+cor(P$close,P$deep_sim_index)
 
 # Scatterplot between two varibales
-plot(P$sim_index,P$close, main="Scatterplot", xlab="Sim_Index ", ylab="Close", col=c("red","blue"),pch=19)
+plot(P$deep_sim_index,P$close, main="Scatterplot", xlab="deep_Sim_Index ", ylab="Close", col=c("red","blue"),pch=19)
 
 # Scatterplot between two varibales
-plot(P$sim_index,P$log_close, main="Scatterplot", xlab="Sim_Index ", ylab="Close", col=c("red","blue"),pch=19)
+plot(P$deep_sim_index,P$log_close, main="Scatterplot", xlab="deep_Sim_Index ", ylab="Close", col=c("red","blue"),pch=19)
 #
-ccf(P$sim_index,P$log_close)
+ccf(P$deep_sim_index,P$close)
 
 
 
@@ -53,23 +53,19 @@ with(DF, plot(time, daily_transcations, type="l", col="red3",
 
 #Plot tow graphs in same plot.
 
-g<- ggplot(DF, aes(time))
-g<- g1 + geom_line(aes(y=close), colour="red")
-g<- g + geom_line(aes(y=sim_index), colour="green")
-g
 
 par(mar = c(5,5,2,5))
 
-with(DF, plot(time, log_close, type="l", col="red3", 
-             ylab="log (Price)",
-             ylim=c(0,3)))
+with(DF, plot(time, close, type="l", col="red3", 
+             ylab="Price",
+             ylim=c(0,250)))
 
 par(new = T)
-with(DF, plot(time, sim_index, type="l", col= "blue", ylim=c(0, 0.1),axes=F, xlab=NA, ylab=NA, cex=1.2))
+with(DF, plot(time, deep_sim_index, type="l", col= "blue", ylim=c(0, 0.1),axes=F, xlab=NA, ylab=NA, cex=1.2))
 axis(side = 4)
-mtext(side = 4, line = 3, 'Similarity Index')
+mtext(side = 4, line = 3, 'Deep Similarity Index')
 legend("topleft",
-       legend=c("Log Price (USD)", "Similarity Metric"),
+       legend=c("Price (USD)", "Deep Similarity Metric"),
        lty=c(2,1), col=c("red3", "blue"))
 
 
